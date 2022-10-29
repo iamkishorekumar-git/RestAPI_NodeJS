ecd0d9da3a52c74869260db40f82173df9f65e7c
iamkishorekumar-git <kishorebca98@gmail.com>
2022-10-29 23:32:28 +0530

    added welcome message in app.js file

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ const ordersRouters = require("./api/routes/orders")
 
 app.use("/products",productRouters)
 app.use("/orders",ordersRouters)
+app.use("/",(req,res,next)=>
+{
+  res.status(200).json({
+    message:"Welcome to rest API creatin using NodeJS"
+  })
+})
 
 module.exports = app
