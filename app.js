bec8482f5e240deb13c6e6b3e30b5ea44251fbb7
KlentyKishore <kishorekumar@klenty.com>
2022-10-29 18:46:02 +0530

    Created server file

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+app = express()
+
+app.use((req,res,next)=>
+{
+    res.status(200).json({
+        message:"Working Fine !!"
+    })
+})
+
+module.exports = app
