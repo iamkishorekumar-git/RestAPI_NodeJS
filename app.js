3296140e5fb08ad90ecbbdb643ad9450717b5ff7
iamkishorekumar-git <kishorebca98@gmail.com>
2022-10-30 08:54:55 +0530

    Added Error handling in app.js

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require("express")
 app = express()
 const productRouters = require("./api/routes/product")
 const ordersRouters = require("./api/routes/orders")
+const morgan = require("morgan")
 
+app.use((morgan("dev")))
 app.use("/products",productRouters)
 app.use("/orders",ordersRouters)
-app.use("/",(req,res,next)=>
+
+app.use((req,res,next)=>
 {
-  res.status(200).json({
-    message:"Welcome to rest API creatin using NodeJS"
+  const error = new Error("Not Found")
+  error.status = 404
+  next(error)
+})
+
+app.use((error,req,res,next)=>
+{
+  res.status(error.status || 500).json({
+    error:{
+      message : error.message
+    }
   })
 })
+
 
 module.exports = app
