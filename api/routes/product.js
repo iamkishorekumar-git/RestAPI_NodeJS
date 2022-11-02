be9fd779ba9cf91cacabd0c4dca64fe9917cbd48
iamkishorekumar-git <kishorebca98@gmail.com>
2022-11-02 12:02:38 +0530

    added update product for Db , and updated readme file

--- api/routes/product.js
+++ api/routes/product.js
@@ -8,7 +8,7 @@ router.get("/",(req,res,next)=>
 {
   Product.find().exec().then((docs)=>
 {
-  if(docs>=1)
+  if(docs)
   {
     res.status(200).json(docs)
   }
@@ -74,9 +74,26 @@ router.get('/:productId',(req,res,next)=>
 
 router.patch('/:productId',(req,res,next)=>
 {
-  res.status(200).json({
-    message :"updated particular productId"
-  })
+ const id = req.params.productId
+
+ const updateOps = {}
+ for(const ops of req.body)
+ {
+   updateOps[ops.propName] = ops.value
+ }
+
+ Product.update({_id:id},{$set: updateOps}).exec().then( result =>
+ {
+   res.status(200).json(result)
+ }).catch(err=>
+ {
+   console.log(err);
+   res.status(500).json({
+     error:err
+   })
+ })
+
+
 })
 
 router.delete('/:productId',(req,res,next)=>
